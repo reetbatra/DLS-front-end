9d29cd934c19bcf5f3db0d6aa905263e7137922d
Reet Batra <reetbatra@Reets-MacBook-Air.local>
2023-02-18 20:37:50 +0530

    Working ethersjs integration

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,21 @@
 import './App.css';
 import { ethers, utils } from "ethers";
 import { useEffect, useState } from 'react';
+import logo from "./DLSlogo.jpeg";
+
+import lottoJSON from "./lotto.json";
+import usdcJSON from "./USDC.json";
+
 
 function App() {
+  const lottoAddress = "0xcad983583f0d5940B3be81d4a89DB474d63C6993";
+  const usdcAddress = "0x1FE38D56D80E388F21ea8b0F6AE2A92C681dA1c3";
+  const lottoABI = lottoJSON["abi"]; 
+  const usdcABI = usdcJSON["abi"];
   const [currentAccount, setCurrentAccount] =  useState('');
   const [chainId, setChainId] = useState(0);
+  const [lottoContract, setLottoContract] = useState(null);
+  const [usdcContract, setUsdcContract] = useState(null);
   const [chainName, setChainName] = useState(null);
   const [balance, setBalance] = useState(0);
   const [currentseries, setcurrentseries] = useState(null);
@@ -23,8 +34,47 @@ function App() {
     setTicketNumber(randomNum);
   }
 
-  const checkIfWalletIsConnected = async () => {
+  const getUSDC = async() => {
+    if(!usdcContract){
+      alert("Metamask not connected!");
+      return;
+    } else {
+      const usdcWithSigner = await usdcContract.connect(signer);
+      const tx = await usdcWithSigner.mint(currentAccount, ethers.utils.parseUnits("10.0", 18));
+      alert("Sent tx");
+      console.log(tx);
+      alert("Usdc minted");
+    }
+  }
 
+  const appoveUSDC = async() => {
+    if(!usdcContract){
+      alert("Metamask not connected!");
+      return;
+    } else {
+      const usdcWithSigner = await usdcContract.connect(signer);
+      const tx = await usdcWithSigner.approve(lottoAddress, ethers.utils.parseUnits("10.0", 18));
+      alert("Sent tx");
+      console.log(tx);
+      alert("Usdc approved");
+    }
+  }
+
+  const buyTicket = async() => {
+    if(!lottoContract){
+      alert("Metamask not connected!");
+      return;
+    } else {
+      const lottoWithSigner = await lottoContract.connect(signer);
+      console.log(series.charCodeAt(0) - 'A'.charCodeAt(0), ticketNumber);
+      const tx = await lottoWithSigner.buyTicket(series.charCodeAt(0) - 'A'.charCodeAt(0), ticketNumber);
+      alert("Sent tx");
+      console.log(tx);
+      alert("Ticket bought");
+    }
+  }
+
+  const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
       if (!ethereum) {
@@ -38,14 +88,11 @@ function App() {
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         provider.getBalance(currentAccount).then((result)=>{
             setBalance(ethers.utils.formatEther(result))
-        })
+        });
         provider.getNetwork().then((result)=>{
-          setChainId(result.chainId)
-          setChainName(result.name)
-        })
-
-        console.log(chainId, chainName, balance)
-
+          setChainId(result.chainId);
+          setChainName(result.name);
+        });
       } else {
         console.log("no authorised account found");
       }
@@ -56,47 +103,95 @@ function App() {
   }
   
 
-const connectWallet = async () => {
-  try {
-    const {ethereum} = window;
-    if(!ethereum) {
-      alert("Get metamask");
-      return;
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+      if(!ethereum) {
+        alert("Get metamask");
+        return;
+      }
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      await setProvider(provider);
+      console.log("Provider:", provider);
+      // MetaMask requires requesting permission to connect users accounts
+      await provider.send("eth_requestAccounts", []);
+
+      // The MetaMask plugin also allows signing transactions to
+      // send ether and pay to change state within the blockchain.
+      // For this, you need the account signer...
+      const signer = provider.getSigner()
+
+      await setSigner(signer);
+      console.log("Signer", signer);
+
+      const lottoContractTemp = new ethers.Contract(lottoAddress, lottoABI, provider);
+      console.log("Lotto contract object", lottoContractTemp);
+      await setLottoContract(lottoContractTemp);
+
+      const usdcContractTemp = new ethers.Contract(usdcAddress, usdcABI, provider);
+      console.log("USDC contract object", usdcContractTemp);
+      await setUsdcContract(usdcContractTemp);
+    } catch (error) {
+      console.log(error);
     }
-    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-    console.log("Connected", accounts[0]);
-    setCurrentAccount(accounts[0]);
-
-  } catch (error) {
-    console.log(error);
   }
-}
 
 
-useEffect(() => {
-  checkIfWalletIsConnected();
-}, [])
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
 
 
   return (
     <div className="App">
       <div className='header-container'>
-        <h1>Kerala Lottery System</h1>
-        <button className='connect-wallet' onClick={connectWallet}>Connect wallet</button>
+        <div>
+          <div className='logo-header'>
+            <div className='logo-text'>
+              <div className='logo-logo'><h1>DLS</h1><h2>Ξ</h2></div>
+              <div><h3>Decentralised Lottery System</h3></div>
+            </div>
+          </div>
+          <button className='connect-wallet' onClick={connectWallet}>Connect wallet</button>
+        </div>
       </div>
       <div className='main-page'>
         <div className='buy-ticket-container'>
-          <p><strong>Why?</strong> <br/> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+          <p><strong>WHY?</strong> <br/> <br/> <ul>
+  <li>Frauds.</li>
+  <li>Lack of transparency.</li>
+  <li>Limited Winners.</li>
+</ul>
+</p>
         </div>
 
         <div className='what-container'>
-          <p><strong>What?</strong><br/>demo text Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum ha</p>
+          <p><strong>WHAT?</strong><br/> <br/>
+          <ul>
+  <li>Transparency.</li>
+  <li>Smart contracts are trustworthy.</li>
+  <li>Winners- new mechanisms.</li>
+  
+</ul>
+          </p>
         </div>
 
         <div className='right-side-container'>
-          <h3>How?</h3>
-            <div className='series-container'> 
+          <div className='how-heading'>
+            <h3>HOW?</h3>
+          </div>
+          <div>
+            <button onClick={getUSDC}>Gimme USDC!  </button>
+          </div>
+          <div>
+            <button onClick={appoveUSDC}>Approve USDC  </button>
+          </div>
+          <div className='series-container'> 
                   <label>Series</label>
                   <select value={series} onChange={(e) =>{
                     setcurrentseries(e.target.value);
@@ -120,13 +215,13 @@ useEffect(() => {
             </div>
 
             <div class Name='submit-button'>
-              
-              <button onClick={""}>Submit</button>
+              <button onClick={buyTicket}>Submit</button>
             </div>
         </div>
       </div>
-
-      <p><a href="https://www.w3schools.com">Click here to know more on this</a></p>
+       <div className='ppt-link'>
+        <a style={{color: "red"}} href="https://www.canva.com/design/DAFa6aXJ_88/XW_kZZfb8rI-__eR8xX4xQ/edit?utm_content=DAFa6aXJ_88&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">Click here to know more on this</a>
+      </div>
     </div>
   );
 }
